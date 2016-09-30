42d3217cf3dc8e6349111c869b39de9c582588a4
ZenBarrier <anthony.j.barrera@gmail.com>
2016-09-30 17:02:55 -0400

    URL Encoding for spaces

--- app/src/main/java/com/zenbarrier/weathergetter/MainActivity.java
+++ app/src/main/java/com/zenbarrier/weathergetter/MainActivity.java
@@ -3,11 +3,18 @@ package com.zenbarrier.weathergetter;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
-import java.net.MalformedURLException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +23,11 @@ public class MainActivity extends AppCompatActivity {
         EditText cityText = (EditText)findViewById(R.id.cityEditText);
 
         String location = cityText.getText().toString().trim();
+        try {
+            location = URLEncoder.encode(location, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
 
         String apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=";
         String apiKey = "&appid="+getResources().getString(R.string.open_weather_api_key);
@@ -33,19 +45,27 @@ public class MainActivity extends AppCompatActivity {
     class WeatherTask extends AsyncTask<String, Void, String>{
         @Override
         protected String doInBackground(String... urls) {
+            String result = "";
             try {
                 URL url = new URL(urls[0]);
-            } catch (MalformedURLException e) {
+                HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+                InputStream inputStream = connection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(inputStream);
+                int data = reader.read();
+                while(data != -1){
+                    result += (char)data;
+                    data = reader.read();
+                }
+            } catch (IOException e) {
                 e.printStackTrace();
             }
-            return null;
+            return result;
         }
 
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
-
-
+            Log.i("Result",s);
         }
     }
 }
