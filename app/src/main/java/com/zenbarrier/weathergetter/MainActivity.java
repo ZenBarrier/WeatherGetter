d0e4f939af31aa72859fe631062b81fcce10f421
ZenBarrier <anthony.j.barrera@gmail.com>
2016-09-30 16:45:11 -0400

    Button creates query

--- app/src/main/java/com/zenbarrier/weathergetter/MainActivity.java
+++ app/src/main/java/com/zenbarrier/weathergetter/MainActivity.java
@@ -3,8 +3,26 @@ package com.zenbarrier.weathergetter;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void getWeather(View view){
+        WeatherTask weatherTask = new WeatherTask();
+        EditText cityText = (EditText)findViewById(R.id.cityEditText);
+
+        String location = cityText.getText().toString().trim();
+
+        String apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=";
+        String apiKey = "&appid="+getResources().getString(R.string.open_weather_api_key);
+        String query = apiUrl+location+apiKey;
+
+        weatherTask.execute(query);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,7 +32,12 @@ public class MainActivity extends AppCompatActivity {
 
     class WeatherTask extends AsyncTask<String, Void, String>{
         @Override
-        protected String doInBackground(String... params) {
+        protected String doInBackground(String... urls) {
+            try {
+                URL url = new URL(urls[0]);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
             return null;
         }
 
@@ -22,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
 
-            
+
         }
     }
 }
