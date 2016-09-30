384c8aebe186927da88fd103caf3ef493e4f23f6
ZenBarrier <anthony.j.barrera@gmail.com>
2016-09-30 16:37:04 -0400

    Async task skeleton

--- app/src/main/java/com/zenbarrier/weathergetter/MainActivity.java
+++ app/src/main/java/com/zenbarrier/weathergetter/MainActivity.java
@@ -1,5 +1,6 @@
 package com.zenbarrier.weathergetter;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    class WeatherTask extends AsyncTask<String, Void, String>{
+        @Override
+        protected String doInBackground(String... params) {
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            
+        }
     }
 }
